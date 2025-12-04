cdcbafc717c2fa448561abd46b2f99b44e37601d
R SANJAY <141536756+RSANJAY21@users.noreply.github.com>
2025-12-04 01:36:40 -0800

    Add files via upload

---
+++ day2/datatypes.java
@@ -0,0 +1,12 @@
+package day2;
+
+public class datatypes {
+	public static void main(String[] args) {
+		int ageofPerson=30;
+		char logo='s';
+		String name="Sanjay";
+		boolean value=true;
+		System.out.println(name);
+		
+	}
+}
---
+++ day2/firstprogram.java
@@ -0,0 +1,7 @@
+package day2;
+
+public class firstprogram {
+	public static void main(String[] args) {
+		System.out.println("First program has created");
+	}
+}
